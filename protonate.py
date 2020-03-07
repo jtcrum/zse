3ee3f064c21c5ec56b84643040e6746fb64a8ebe
jtcrum <jcrum@nd.edu>
2020-03-07 14:33:34 -0500

    fixed paired and updated examples

--- protonate.py
+++ protonate.py
@@ -71,34 +71,34 @@ def isolated(atoms,tatom,path = '.'):
         traj += [Atoms(H_lattice)]
 
         if not os.path.exists(path+'/D-' + str(first_oxygens[l])):
-            os.mkdir(path+'/D-' + str(first_oxygens[l]))
+            os.makedirs(path+'/D-' + str(first_oxygens[l]))
         write(path+'/D-'  + str(first_oxygens[l]) +'/POSCAR',H_lattice, sort = True)
 
-    return traj
+    return traj , oxygens
 
 def paired(atoms, tatoms, path = '.'):
-        '''
-        atoms should be an ase atoms object
-        tatoms should be the indices of the hetero atoms with which you want to place
-        hydrogen atoms around
-        alternatively, tatoms can be a symbol i.e. 'Al' if both hetero atoms are the same element
-        path is the location you want to save the new structures
-        new structures are saved with the format of:
-        D-INDEX OF OXYGEN THAT H IS BOUND TO
-        '''
+    '''
+    atoms should be an ase atoms object
+    tatoms should be the indices of the hetero atoms with which you want to place
+    hydrogen atoms around
+    alternatively, tatoms can be a symbol i.e. 'Al' if both hetero atoms are the same element
+    path is the location you want to save the new structures
+    new structures are saved with the format of:
+    D-INDEX OF OXYGEN THAT H IS BOUND TO
+    '''
     lattice = atoms.copy()
-    total_oxygen = [atom.index for atom in atoms if atom.symnbol == 'O']
+    total_oxygen = [atom.index for atom in atoms if atom.symbol == 'O']
 
     if isinstance(tatoms,list):
         tsites = tatoms
     elif isinstance(tatoms, str):
-        tsites = [atom.index for atom in atoms if atom.symmbol == tatoms]
+        tsites = [atom.index for atom in atoms if atom.symbol == tatoms]
 
     total_silicon = [atom.index for atom in atoms if atom.symbol == 'Si']
     n = len(atoms)
     Hydrogen = []
     for i in range(n):
-        Hydorgen.append(len(atoms)+i)
+        Hydrogen.append(len(atoms)+i)
 
     oxygens = []
     for j in tsites:
@@ -140,40 +140,42 @@ def paired(atoms, tatoms, path = '.'):
 
     cwd = os.getcwd()
 
-    First_Oxygens=First_Oxygens.astype(int)
-    First_Silicons = First_Silicons.astype(int)
-    Aluminium = np.array(Aluminium)
-    Aluminium = Aluminium.astype(int)
+    first_oxygens=first_oxygens.astype(int)
+    first_silicons = first_silicons.astype(int)
+    tsites = np.array(tsites)
+    tsites = tsites.astype(int)
     Hydrogen = np.array(Hydrogen)
     Hydrogen = Hydrogen.astype(int)
     traj = []
     for l in range(4):
         center = H_lattice.get_center_of_mass()
         positions = atoms.get_positions()
-        diff = center - positions[Aluminium[0]]
+        diff = center - positions[tsites[0]]
         H_lattice.translate(diff)
         H_lattice.wrap()
-        first_distance = H_lattice.set_distance(First_Oxygens[l], Hydrogen[0], 0.98, fix=0)
-        first_Al_angle = H_lattice.set_angle(int(Aluminium[0]), First_Oxygens[l], Hydrogen[0], 109.6, mask=None)
-        first_Si_angle = H_lattice.set_angle(int(First_Silicons[l]), First_Oxygens[l], Hydrogen[0], 109.6, mask=None)
-        first_dihedral = H_lattice.set_dihedral(int(Aluminium[0]), First_Oxygens[l], First_Silicons[l], Hydrogen[0], 180, mask=None)
+        first_distance = H_lattice.set_distance(first_oxygens[l], Hydrogen[0], 0.98, fix=0)
+        first_Al_angle = H_lattice.set_angle(int(tsites[0]), first_oxygens[l], Hydrogen[0], 109.6, mask=None)
+        first_Si_angle = H_lattice.set_angle(int(first_silicons[l]), first_oxygens[l], Hydrogen[0], 109.6, mask=None)
+        first_dihedral = H_lattice.set_dihedral(int(tsites[0]), first_oxygens[l], first_silicons[l], Hydrogen[0], 180, mask=None)
         H_lattice.translate(-1*diff)
         H_lattice.wrap()
         for k in range(4):
             center = H_lattice.get_center_of_mass()
             positions = atoms.get_positions()
-            diff = center - positions[Aluminium[1]]
+            diff = center - positions[tsites[1]]
             H_lattice.translate(diff)
             H_lattice.wrap()
-            second_distance = H_lattice.set_distance(Second_Oxygens[k], Hydrogen[1], 0.98, fix=0)
-            second_Al_angle = H_lattice.set_angle(int(Aluminium[1]), Second_Oxygens[k], Hydrogen[1], 109.6, mask=None)
-            second_Si_angle = H_lattice.set_angle(int(Second_Silicons[k]), Second_Oxygens[k], Hydrogen[1], 109.6, mask=None)
-            second_dihedral = H_lattice.set_dihedral(int(Aluminium[1]), Second_Oxygens[k], Second_Silicons[k], Hydrogen[1], 180, mask=None)
+            second_distance = H_lattice.set_distance(second_oxygens[k], Hydrogen[1], 0.98, fix=0)
+            second_Al_angle = H_lattice.set_angle(int(tsites[1]), second_oxygens[k], Hydrogen[1], 109.6, mask=None)
+            second_Si_angle = H_lattice.set_angle(int(second_silicons[k]), second_oxygens[k], Hydrogen[1], 109.6, mask=None)
+            second_dihedral = H_lattice.set_dihedral(int(tsites[1]), second_oxygens[k], second_silicons[k], Hydrogen[1], 180, mask=None)
             H_lattice.translate(-1*diff)
             H_lattice.wrap()
             # makes directory of each First_Oxygen-Second_Oxygen case with terminal hydrogen
             if not os.path.exists('{0}/D-{1}-{2}'.format(path,str(first_oxygens[l]),str(second_oxygens[k]))):
-                os.mkdir('{0}/D-{1}-{2}'.format(path,str(first_oxygens[l]),str(second_oxygens[k])))
+                os.makedirs('{0}/D-{1}-{2}'.format(path,str(first_oxygens[l]),str(second_oxygens[k])))
 
             write('{0}/D-{1}-{2}/POSCAR'.format(path,str(first_oxygens[l]),str(second_oxygens[k])),H_lattice, sort = True)
             traj += [Atoms(H_lattice)]
+
+    return traj , oxygens
