818a227737b421224b3121edfced7e6cb6e3205f
jtcrum <jcrum@nd.edu>
2020-08-25 15:01:10 -0400

    updated cation.py

--- cation.py
+++ cation.py
@@ -6,9 +6,43 @@ import numpy as np
 import os
 from ase.build import molecule
 
+def monovalent(atoms,symbol,path=None):
+    total_oxygen = [atom.index for atom in atoms if atom.symbol == 'O']
+    aluminum = [atom.index for atom in atoms if atom.symbol == 'Al']
+    cation = [len(atoms)]
+    oxygens = []
+    for j in aluminum:
+        tmp = []
+        for k in total_oxygen:
+            distance = atoms.get_distance(k,j,mic=True)
+            if distance < 1.7:
+                tmp.append(k)
+                if len(tmp)  == 4:
+                    oxygens.append(tmp)
+                    tmp = []
+
+    oxygens = np.array(oxygens)
+    traj = []
+
+    for l in range(len(aluminum)):
+        for i in range(len(oxygens[l,:])):
+            for j in (x for x in range(len(oxygens[l,:])) if x > i):
+                totpos = atoms.get_positions()
+                a = totpos[oxygens[l,i],:]
+                b = totpos[oxygens[l,j],:]
+                c = totpos[aluminum[l],:]
+                pos = a-c+b
+                tpos = pos.reshape(1,3)
+                adsorbate = Atoms(M)
+                adsorbate.set_positions(tpos)
+                M_lattice = atoms+adsorbate
+                traj+=[M_lattice]
+
+return traj
+
 def divalent(atoms,M,path = None):
     '''
-    This function ill place one divalent cation into a zeolite framework that
+    This function will place one divalent cation into a zeolite framework that
     contains two negative charge centers.
     The cation is placed at each of 6 rings around each of the aluminum atoms.
     This creates 12 structures. Depending on the placement of your Al, some of
