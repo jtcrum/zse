00bdbc84a686f22a1d288ae6dff4babde072dbfc
jtcrum <jcrum@nd.edu>
2020-10-05 10:10:23 -0400

    updated readme.org

--- cation.py
+++ cation.py
@@ -6,45 +6,6 @@ import numpy as np
 import os
 from ase.build import molecule
 
-def monovalent(atoms,symbol,path=None):
-    total_oxygen = [atom.index for atom in atoms if atom.symbol == 'O']
-    aluminum = [atom.index for atom in atoms if atom.symbol == 'Al']
-    cation = [len(atoms)]
-    oxygens = []
-    for j in aluminum:
-        tmp = []
-        for k in total_oxygen:
-            distance = atoms.get_distance(k,j,mic=True)
-            if distance < 2:
-                tmp.append(k)
-                if len(tmp)  == 4:
-                    oxygens.append(tmp)
-                    tmp = []
-
-    oxygens = np.array(oxygens)
-    traj = []
-
-    for l in range(len(aluminum)):
-        for i in range(len(oxygens[l,:])):
-            for j in (x for x in range(len(oxygens[l,:])) if x > i):
-                totpos = atoms.get_positions()
-                a = totpos[oxygens[l,i],:]
-                b = totpos[oxygens[l,j],:]
-                c = totpos[aluminum[l],:]
-                pos = a-c+b
-                tpos = pos.reshape(1,3)
-                adsorbate = Atoms(symbol)
-                adsorbate.set_positions(tpos)
-                M_lattice = atoms+adsorbate
-                traj+=[M_lattice]
-
-                if path:
-                    os.makedirs('{0}/D-{1}-{2}-{3}'.format(path,str(aluminum[l]),str(oxygens[l,j]),str(oxygens[l,i])),exist_ok=True)
-
-                    write('{0}/D-{1}-{2}-{3}/POSCAR'.format(path,str(aluminum[l]),str(oxygens[l,j]),str(oxygens[l,i])),M_lattice, sort = True)
-
-    return traj
-
 def divalent(atoms,M,path = None):
     '''
     This function will place one divalent cation into a zeolite framework that
@@ -94,4 +55,55 @@ def divalent(atoms,M,path = None):
                     os.makedirs('{0}/D-{1}-{2}-{3}'.format(path,str(aluminum[l]),str(oxygens[l,j]),str(oxygens[l,i])))
 
                     write('{0}/D-{1}-{2}-{3}/POSCAR'.format(path,str(aluminum[l]),str(oxygens[l,j]),str(oxygens[l,i])),M_lattice, sort = True)
+    return traj
+
+def monovalent(atoms,symbol,path=None):
+
+    '''
+    This function will place one monovalent cation into a zeolite framework that
+    contains one aluminum atom.
+    The cation is placed at each of 6 rings around the aluminum atoms.
+    This creates 6 structures.
+
+    The structures will be placed in the path provided as an input.
+    Format of the structures folder names are:
+    D-aluminum_index-oxygen1_index,oxygen2_index.
+    '''
+
+    total_oxygen = [atom.index for atom in atoms if atom.symbol == 'O']
+    aluminum = [atom.index for atom in atoms if atom.symbol == 'Al']
+    cation = [len(atoms)]
+    oxygens = []
+    for j in aluminum:
+        tmp = []
+        for k in total_oxygen:
+            distance = atoms.get_distance(k,j,mic=True)
+            if distance < 2:
+                tmp.append(k)
+                if len(tmp)  == 4:
+                    oxygens.append(tmp)
+                    tmp = []
+
+    oxygens = np.array(oxygens)
+    traj = []
+
+    for l in range(len(aluminum)):
+        for i in range(len(oxygens[l,:])):
+            for j in (x for x in range(len(oxygens[l,:])) if x > i):
+                totpos = atoms.get_positions()
+                a = totpos[oxygens[l,i],:]
+                b = totpos[oxygens[l,j],:]
+                c = totpos[aluminum[l],:]
+                pos = a-c+b
+                tpos = pos.reshape(1,3)
+                adsorbate = Atoms(symbol)
+                adsorbate.set_positions(tpos)
+                M_lattice = atoms+adsorbate
+                traj+=[M_lattice]
+
+                if path:
+                    os.makedirs('{0}/D-{1}-{2}-{3}'.format(path,str(aluminum[l]),str(oxygens[l,j]),str(oxygens[l,i])),exist_ok=True)
+
+                    write('{0}/D-{1}-{2}-{3}/POSCAR'.format(path,str(aluminum[l]),str(oxygens[l,j]),str(oxygens[l,i])),M_lattice, sort = True)
+
     return traj
