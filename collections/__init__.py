744fa5c183f93b8eabd57f6bad13daa7fc104d65
jtcrum <jcrum@nd.edu>
2021-01-06 09:31:49 -0700

    fixed missing import

--- collections/__init__.py
+++ collections/__init__.py
@@ -2,5 +2,6 @@ from zse.collections.framework import framework
 from zse.collections.framework import get_fwrings
 from zse.collections.framework import get_tsites
 from zse.collections.framework import get_all_fws
+from zse.collections.framework import get_osites
 
 __all__ = ['framework','get_fwrings','get_tsites','get_osites','get_all_fws']
