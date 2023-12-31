b8b2aea460f57d45fbed60577a727f6d0b9f861e
Andrew Rosen <asrosen93@gmail.com>
2023-12-31 15:27:24 -0800

    Fix test

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from ase.io import read, write
+from ase.io import read
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 
 from zse.substitute import exchange_unique_T_sites
 from zse.utilities import make_iza_zeolite
@@ -8,19 +9,21 @@ from zse.utilities import make_iza_zeolite
 REF_DATA = Path(__file__).parent / Path("data")
 
 
-def test_all_silica(monkeypatch, tmp_path):
-    monkeypatch.chdir(tmp_path)
+def test_all_silica():
     atoms = make_iza_zeolite("CHA")
-    write("CHA.cif", atoms)
+    ref_atoms = read(f"{REF_DATA}/CHA.cif")
+    assert_array_equal(atoms.get_chemical_symbols(), ref_atoms.get_chemical_symbols())
+    assert_array_almost_equal(atoms.get_positions(), ref_atoms.get_positions())
+    assert_array_almost_equal(atoms.get_cell(), ref_atoms.get_cell())
 
-    assert read("CHA.cif") == read(f"{REF_DATA}/CHA.cif")
 
-
-def test_exchange_unique(monkeypatch, tmp_path):
-    monkeypatch.chdir(tmp_path)
-
+def test_exchange_unique():
     zeolite = read(f"{REF_DATA}/CHA.cif")
     exchanged_zeolites = exchange_unique_T_sites(zeolite, "CHA", "B", "Na")
     assert len(exchanged_zeolites) == 4
-    write("CHA_B_Na_3.cif", exchanged_zeolites[3])
-    assert read("CHA_B_Na_3.cif") == read(f"{REF_DATA}/MOR_B_Na_3.cif")
+
+    atoms = exchanged_zeolites[3]
+    ref_atoms = read(f"{REF_DATA}/MOR_B_Na_3.cif")
+    assert_array_equal(atoms.get_chemical_symbols(), ref_atoms.get_chemical_symbols())
+    assert_array_almost_equal(atoms.get_positions(), ref_atoms.get_positions())
+    assert_array_almost_equal(atoms.get_cell(), ref_atoms.get_cell())
