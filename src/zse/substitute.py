59f81421a11946e4b77dbca0a3f988fae8139479
Andrew Rosen <asrosen93@gmail.com>
2023-12-31 17:50:39 -0800

    Fix

--- src/zse/substitute.py
+++ src/zse/substitute.py
@@ -14,6 +14,59 @@ if TYPE_CHECKING:
     from typing import Any
 
 
+def tsub(atoms, index, new_atom):
+    """
+    atoms should be an ase atoms object
+    index is the index of the atom(s) you would like to substitute
+    new_atom is the elemental symbol of the atom you want to replace index with.
+    """
+    z = deepcopy(atoms)
+    symbols = z.get_chemical_symbols()
+    if isinstance(index, int):
+        index = [index]
+    for i in index:
+        symbols[i] = new_atom
+
+    z.set_chemical_symbols(symbols)
+
+    return z
+
+
+def nest(atoms, index):
+    z = deepcopy(atoms)
+
+    position = z[index].position  # position of that t site
+
+    # This centers the atom object on the T site we want to remove
+    center = z.get_center_of_mass()
+    trans = center - position
+    z.translate(trans)
+    z.wrap()
+
+    # get the neighbor list
+    cutoff = neighborlist.natural_cutoffs(z, mult=1.05)
+    nl = neighborlist.NeighborList(
+        cutoffs=cutoff, self_interaction=False, bothways=True
+    )
+    nl.update(z)
+    oxygens = nl.get_neighbors(index)[0]
+
+    # add a hydrogen next to each neighbor oxygen
+
+    for o in oxygens:
+        vector = position - z[o].position
+        hyd = molecule("H")
+        new_location = z[o].position + vector / (vector**2).sum() ** 0.5
+        hyd.translate(new_location)
+        z = z + hyd
+
+    # recenter the atoms back to their original position and delete the Si
+    z.translate(-trans)
+    z.wrap()
+    del z[index]
+    return z
+
+
 def exchange_unique_T_sites(
     zeolite: Atoms,
     code: str,
@@ -86,56 +139,3 @@ def exchange_unique_T_sites(
                     continue
             zeolites.append(exchanged_zeolite)
     return zeolites
-
-
-def tsub(atoms, index, new_atom):
-    """
-    atoms should be an ase atoms object
-    index is the index of the atom(s) you would like to substitute
-    new_atom is the elemental symbol of the atom you want to replace index with.
-    """
-    z = deepcopy(atoms)
-    symbols = z.get_chemical_symbols()
-    if isinstance(index, int):
-        index = [index]
-    for i in index:
-        symbols[i] = new_atom
-
-    z.set_chemical_symbols(symbols)
-
-    return z
-
-
-def nest(atoms, index):
-    z = deepcopy(atoms)
-
-    position = z[index].position  # position of that t site
-
-    # This centers the atom object on the T site we want to remove
-    center = z.get_center_of_mass()
-    trans = center - position
-    z.translate(trans)
-    z.wrap()
-
-    # get the neighbor list
-    cutoff = neighborlist.natural_cutoffs(z, mult=1.05)
-    nl = neighborlist.NeighborList(
-        cutoffs=cutoff, self_interaction=False, bothways=True
-    )
-    nl.update(z)
-    oxygens = nl.get_neighbors(index)[0]
-
-    # add a hydrogen next to each neighbor oxygen
-
-    for o in oxygens:
-        vector = position - z[o].position
-        hyd = molecule("H")
-        new_location = z[o].position + vector / (vector**2).sum() ** 0.5
-        hyd.translate(new_location)
-        z = z + hyd
-
-    # recenter the atoms back to their original position and delete the Si
-    z.translate(-trans)
-    z.wrap()
-    del z[index]
-    return z
