058403a429c39d347c3d9f2502e03f437e1edb08
alex <ahoffman2011@gmail.com>
2025-09-14 21:42:47 -0400

    DOC: add typing and docstrings for ring_utilities.py

--- zse/ring_utilities.py
+++ zse/ring_utilities.py
@@ -1,13 +1,11 @@
-"""
-This module contains utilities to be used by the rings.py module.
-"""
+"""This module contains utilities to be used by the rings.py module."""
 
-import math
 from collections import defaultdict as dd
 from itertools import permutations as perm
 
 import networkx as nx
 import numpy as np
+from ase import Atoms
 from ase.geometry import get_distances
 
 from zse.utilities import center
@@ -23,21 +21,24 @@ __all__ = [
     "remove_dups",
     "remove_geometric_dups",
     "remove_labeled_dups",
-    "remove_non_rings",
     "shortest_valid_path",
     "vertex_order",
 ]
 
 
-def atoms_to_graph(atoms, index, max_ring):
-    """
-    Helper function to repeat a unit cell enough times to capture the largest
+def atoms_to_graph(atoms: Atoms, index: int, max_ring: int) -> tuple[nx.Graph, Atoms, list[int]]:
+    """Repeat a unit cell enough times to capture the largest
     possible ring, and turn the new larger cell into a graph object.
 
-    RETURNS:
-    G = graph object representing zeolite framework in new larger cell
-    large_atoms = ASE atoms object of the new larger cell framework
-    repeat = array showing the number of times the cell was repeated: [x,y,z]
+    Args:
+        atoms (Atoms): ASE atoms object of the zeolite framework
+        index (int): index of the T atom to be analyzed
+        max_ring (int): size of the largest ring to be analyzed
+
+    Returns:
+        graph (nx.Graph): graph object representing zeolite framework in new larger cell
+        large_atoms (Atoms): ASE atoms object of the new larger cell framework
+        repeat (list[int]): array showing the number of times the cell was repeated: [x,y,z]
     """
 
     # repeat cell, center the cell, and wrap the atoms back into the cell
@@ -95,9 +96,14 @@ def atoms_to_graph(atoms, index, max_ring):
     return graph, large_atoms, repeat
 
 
-def remove_dups(paths):
-    """
-    This is a helper function for get_orings and get_trings.
+def remove_dups(paths: list) -> list:
+    """A helper function for get_orings and get_trings to remove duplicate paths.
+
+    Args:
+        paths (list): list of lists containing the paths
+
+    Returns:
+        paths (list): list of lists containing the paths with duplicates removed
     """
     d = []
     for i in range(len(paths)):
@@ -107,15 +113,21 @@ def remove_dups(paths):
                 st2 = set(paths[j])
                 if st1 == st2:
                     d.append(int(j))
-    tmp_paths = []
-    for i in range(len(paths)):
-        if i not in d:
-            tmp_paths.append(paths[i])
+    tmp_paths = [paths[i] for i in range(len(paths)) if i not in d]
     paths = tmp_paths
     return paths
 
 
-def paths_to_atoms(atoms, paths):
+def paths_to_atoms(atoms: Atoms, paths: list) -> Atoms:
+    """Convert a list of paths (lists of indices) to an ASE atoms object.
+
+    Args:
+        atoms (Atoms): ASE atoms object of the zeolite framework
+        paths (list): list of lists containing the paths
+
+    Returns:
+        tmp_atoms (Atoms): ASE atoms object containing only the atoms in the paths
+    """
     keepers = []
     for i in paths:
         for j in i:
@@ -128,13 +140,23 @@ def paths_to_atoms(atoms, paths):
     return tmp_atoms
 
 
-def remove_geometric_dups(atoms, paths):
+def remove_geometric_dups(atoms: Atoms, paths: list) -> list:
+    """A helper function for get_orings and get_trings to remove geometrically
+    duplicate paths.
+
+    Args:
+        atoms (Atoms): ASE atoms object of the zeolite framework
+        paths (list): list of lists containing the paths
+
+    Returns:
+        unique_paths (list): list of lists containing the paths with geometric duplicates removed
+    """
     unique_paths = []
     unique_distances = []
     for p in paths:
         atoms, _trans = center(atoms, p[0])
         dists = []
-        for x in range(0, len(p) - 1):
+        for x in range(len(p) - 1):
             for r in range(x + 1, len(p)):
                 dist = round(atoms.get_distance(p[x], p[r]), 2)
                 dists.append(dist)
@@ -146,47 +168,82 @@ def remove_geometric_dups(atoms, paths):
     return unique_paths
 
 
-def get_vertices(graph, index):
+def get_vertices(graph: nx.Graph, index: int) -> list:
+    """Get all the vertices (pairs of neighbors) connected to the index atom.
+
+    Args:
+        graph (nx.Graph): graph object representing zeolite framework in new larger cell
+        index (int): index of the T atom to be analyzed
+
+    Returns:
+        vertices (list): list of lists containing the vertices (pairs of neighbors)
+    """
     vertices = []
-    neighbors = [n for n in nx.neighbors(graph, index)]
+    neighbors = list(nx.neighbors(graph, index))
     length = len(neighbors)
     for j in range(length - 1):
         for k in range(j + 1, length):
-            vertices.append([neighbors[j], neighbors[k]])
+            vertices.append([neighbors[j], neighbors[k]])  # noqa: PERF401
     return vertices
 
 
-def shortest_valid_path(graph, o1, o2, index, l):
+def shortest_valid_path(
+    graph: nx.Graph, o1: int, o2: int, index: int, length: int
+) -> tuple[list, int]:
+    """Find the shortest valid path between two oxygen atoms that goes through
+    the index atom.
+
+    Args:
+        graph (nx.Graph): graph object representing zeolite framework in new larger cell
+        o1 (int): index of the first oxygen atom
+        o2 (int): index of the second oxygen atom
+        index (int): index of the T atom to be analyzed
+        l (int): maximum length of the path
+
+    Returns:
+        p (list): list containing the shortest valid path
+        length (int): length of the shortest valid path
+    """
     graph_ = graph.copy()
     graph_.remove_node(index)
     flag = True
     l2 = 6
-    p = []
+    p_idx = []
     while flag:
         paths = nx.all_simple_paths(graph_, o1, o2, l2 - 1)
-        for p in paths:
-            p.append(index)
-            if len(p) == l2:
-                flag, _j = is_valid(graph, p)
+        for path_ in paths:
+            p_idx.append(index)
+            if len(path_) == l2:
+                flag, _j = is_valid(graph, path_)
             if not flag:
                 break
         if flag:
             l2 += 2
-        if l2 > l:
-            p = [1]
+        if l2 > length:
+            p_idx = [1]
             break
-    return p, len(p)
+    return p_idx, len(p_idx)
 
 
-def is_valid(G, path):
-    l = len(path)
+def is_valid(graph: nx.Graph, path: list) -> tuple[bool, int]:
+    """Check if a path is valid (i.e., does not have shortcuts).
+
+    Args:
+        graph (nx.Graph): graph object representing zeolite framework in new larger cell
+        path (list): list containing the path to be checked
+
+    Returns:
+        flag (bool): True if the path is not valid, False if it is valid
+        j (int): index of the first node in the path that creates a shortcut
+    """
+    path_length = len(path)
     flag = False
-    for j in range(1, l - 1, 2):
-        for k in range(j + 2, l, 2):
+    for j in range(1, path_length - 1, 2):
+        for k in range(j + 2, path_length, 2):
             node = path[j]
-            sp = nx.shortest_path(G, node, path[k])
+            sp = nx.shortest_path(graph, node, path[k])
 
-            if len(sp) < k - j + 1 and len(sp) < l - (k - j) + 1:
+            if len(sp) < k - j + 1 and len(sp) < path_length - (k - j) + 1:
                 flag = True
                 break
         if flag:
@@ -194,23 +251,43 @@ def is_valid(G, path):
     return flag, j
 
 
-def all_paths(G, o1, o2, index, l):
-    import networkx as nx
+def all_paths(graph: nx.Graph, o1: int, o2: int, index: int, path_length: int) -> list:
+    """Find all valid paths between two oxygen atoms that go through
+    the index atom.
 
+    Args:
+        graph (nx.Graph): graph object representing zeolite framework in new larger cell
+        o1 (int): index of the first oxygen atom
+        o2 (int): index of the second oxygen atom
+        index (int): index of the T atom to be analyzed
+        path_length (int): length of the path
+
+    Returns:
+        all_paths (list): list of lists containing all valid paths
+    """
     all_paths = []
-    G2 = G.copy()
-    G2.remove_node(index)
-    paths = nx.all_simple_paths(G2, o1, o2, l)
+    graph_ = graph.copy()
+    graph_.remove_node(index)
+    paths = nx.all_simple_paths(graph_, o1, o2, path_length)
     for path in paths:
         path.append(index)
-        if len(path) == l:
-            flag, j = is_valid(G, path)
+        if len(path) == path_length:
+            flag, _j = is_valid(graph, path)
             if not flag and path not in all_paths:
                 all_paths.append(path)
     return all_paths
 
 
-def vertex_order(r):
+def vertex_order(r: list) -> tuple[str, list]:
+    """Order the paths based on the oxygen pairs and their weights.
+
+    Args:
+        r (list): list of lists containing the paths
+
+    Returns:
+        ordered_v (str): string representing the ordered vertices
+        new_r (list): list of lists containing the ordered paths
+    """
     oxygens = []
     o_pair_paths = dd(list)
     o_pair_sizes = dd(int)
@@ -228,7 +305,7 @@ def vertex_order(r):
         o_pair_counts["-".join(oxys)] += 1
         o_pair_weights["-".join(oxys)] += len(path)
 
-    perms = [x for x in perm(oxygens)]
+    perms = list(perm(oxygens))
     weights = []
     order = [[0, 2], [0, 1], [1, 2], [2, 3], [3, 0], [1, 3]]
     for p in perms:
@@ -237,13 +314,13 @@ def vertex_order(r):
             try:
                 k = "-".join(sorted([str(p[o[0]]), str(p[o[1]])]))
                 w.append(o_pair_weights[k])
-            except:
+            except:  # noqa: E722, PERF203
                 pass
         weights.append(w)
 
-    zipped_lists = zip(weights, perms)
+    zipped_lists = zip(weights, perms, strict=False)
     sp = sorted(zipped_lists, reverse=True)
-    tuples = zip(*sp)
+    tuples = zip(*sp, strict=False)
     weights, perms = [list(tuple) for tuple in tuples]
 
     new_r = []
@@ -258,25 +335,39 @@ def vertex_order(r):
             sizes.append(o_pair_sizes[k])
             counts.append(o_pair_counts[k])
             for x in o_pair_paths[k]:
-                new_r.append(x)
-        except:
+                new_r.append(x)  # noqa: PERF402
+        except:  # noqa: E722, PERF203
             pass
 
     ordered_v = []
-    for c, s in zip(counts, sizes):
+    for c, s in zip(counts, sizes, strict=False):
         if c == 1:
-            ordered_v.append("{0}".format(int(s / 2)))
+            ordered_v.append(f"{int(s / 2)}")
         elif c == 0:
             ordered_v.append("*")
         else:
-            ordered_v.append("{0}_{1}".format(int(s / 2), c))
+            ordered_v.append(f"{int(s / 2)}_{c}")
     ordered_v = "•".join(ordered_v)
     c = [int(len(x) / 2) for x in new_r]
 
     return ordered_v, new_r
 
 
-""" DEPRECATED FUNCTIONS """
+def dict_to_atoms(index_paths, atoms):
+    trajectories = {}
+    com = atoms.get_center_of_mass()
+    for length in index_paths:
+        paths = index_paths[length]
+        tmp_traj = []
+        for p in paths:
+            tmp_atoms = paths_to_atoms(atoms, [p])
+            trans = com - tmp_atoms[0].position
+            tmp_atoms.translate(trans)
+            tmp_atoms.wrap()
+            tmp_traj += [tmp_atoms]
+        trajectories[length] = tmp_traj
+
+    return trajectories
 
 
 def remove_labeled_dups(index_paths, label_paths, ring_sizes, atoms):
@@ -304,7 +395,7 @@ def remove_labeled_dups(index_paths, label_paths, ring_sizes, atoms):
                 st2_2 = " ".join(map(str, reversed(ring_tlist[j])))
                 if st2 in st1 + " " + st1 or st2_2 in st1 + " " + st1:
                     p = ring_full[i]
-                    atoms, trans = center(atoms, p[0])
+                    atoms, _trans = center(atoms, p[0])
                     cross1 = []
                     for x in range(1, len(p) + 1, 2):
                         for r in range(x + 2, len(p) + 1, 2):
@@ -313,7 +404,7 @@ def remove_labeled_dups(index_paths, label_paths, ring_sizes, atoms):
                     cross1.sort()
 
                     p = ring_full[j]
-                    atoms, trans = center(atoms, p[0])
+                    atoms, _trans = center(atoms, p[0])
                     cross2 = []
                     for x in range(1, len(p) + 1, 2):
                         for r in range(x + 2, len(p) + 1, 2):
@@ -335,157 +426,25 @@ def remove_labeled_dups(index_paths, label_paths, ring_sizes, atoms):
     return index_rings, label_rings
 
 
-def get_paths(G, index, ring_sizes):
-    """
-    Get all the paths (rings) between the index atom and its neighbor
+def get_paths(graph: nx.Graph, index: int, ring_sizes: list) -> list:
+    """Get all the paths (rings) between the index atom and its neighbor.
+
+    Args:
+        graph (nx.Graph): graph object representing zeolite framework in new larger cell
+        index (int): index of the T atom to be analyzed
+        ring_sizes (list): list of integers representing the sizes of rings to be analyzed
+
+    Returns:
+        paths (list): list of lists containing the paths
     """
 
-    # first find the neighbor
-    import networkx as nx
-
-    neighbors = [n for n in nx.neighbors(G, index)]
+    neighbors = list(nx.neighbors(graph, index))
     neighbor = neighbors[0]
 
     # next find all paths connecting index and neighbor
     paths = []
-    for path in nx.all_simple_paths(G, index, neighbor, cutoff=max(ring_sizes) - 1):
+    for path in nx.all_simple_paths(graph, index, neighbor, cutoff=max(ring_sizes) - 1):
         if len(path) in ring_sizes:
-            paths.append(path)
+            paths.append(path)  # noqa: PERF401
 
     return paths
-
-
-def remove_sec(paths):
-    """
-    This is a helper function for get_orings and get_trings.
-    """
-    d = []
-    count2 = np.zeros(len(paths))
-
-    for i in range(len(paths)):
-        for j in range(i + 1, len(paths)):
-            if i != j:
-                ringi = paths[i]
-                ringj = paths[j]
-                ni = len(ringi)
-                nj = len(ringj)
-                if ni > nj and ni >= 16 and nj > 6:
-                    count = 0
-                    for rj in ringj:
-                        if rj in ringi:
-                            count += 1
-                    if count == nj / 2:
-                        count2[i] += 1
-                    elif count > nj / 2:
-                        count2[i] += 2
-                if nj > ni and nj >= 16 and ni > 6:
-                    count = 0
-                    for ri in ringi:
-                        if ri in ringj:
-                            count += 1
-                    if count == ni / 2:
-                        count2[j] += 1
-                    elif count > ni / 2:
-                        count2[j] += 2
-                if ni > nj and nj in [6, 8]:
-                    count = 0
-                    for rj in ringj:
-                        if rj in ringi:
-                            count += 1
-                    if count >= nj - 2:
-                        count2[i] += 2
-                if nj > ni and ni in [6, 8]:
-                    count = 0
-                    for ri in ringi:
-                        if ri in ringj:
-                            count += 1
-                    if count >= ni - 2:
-                        count2[j] += 2
-    for i, c in enumerate(count2):
-        if c >= 2:
-            d.append(i)
-    tmp_paths = []
-    for i in range(len(paths)):
-        if i not in d:
-            tmp_paths.append(paths[i])
-    paths = tmp_paths
-    return paths
-
-
-def remove_non_rings(atoms, paths):
-    # turn each path into a circular array to find and remove any duplicates
-    paths = remove_dups(paths)
-
-    # remove paths that contain smaller rings becuase these are likely not
-    # actual rings
-    # there are a ton of rules I have written to find these "secondary rings"
-    # there is probably a better way to do this, I would love other's input.
-
-    """
-    This portion doesn't seem to be necessary anymore.
-    paths = remove_sec(paths)
-    """
-    # this part is a trick to cut out some of the paths that are just
-    # random walks through the framework, and not actual rings
-    # here we have some cutoff distance for cross ring T-T distance
-    # if the distance is less than the cutoff it probably isn't a ring
-    # for odd MR we need a different function so there is no cross ring T-T
-
-    delete = []
-    for j, r in enumerate(paths):
-        n = int(len(r) / 2)
-        cutoff = n / 4
-        if cutoff < 2:
-            cutoff = 2
-        if n % 2 == 0 and n > 5:
-            distances = []
-            inner_flag = False
-            for x in range(1, n, 2):
-                dist = atoms.get_distance(r[x], r[x + n], mic=True)
-                distances.append(dist)
-                if dist < n - cutoff:
-                    delete.append(j)
-                    inner_flag = True
-                    break
-            if inner_flag == False:
-                outer_flag = False
-                for d in distances:
-                    if d > n - math.floor(n / 6):
-                        outer_flag = True
-                        break
-                if outer_flag == False:
-                    delete.append(j)
-        if n % 2 != 0 and n > 5:
-            r2 = r.copy()
-            r2.append(r[:2])
-            for x in range(1, n, 2):
-                dist1 = atoms.get_distance(r2[x], r2[x + n - 1], mic=True)
-                dist2 = atoms.get_distance(r2[x], r2[x + n + 1], mic=True)
-                if dist1 < n - cutoff or dist2 < n - cutoff:
-                    delete.append(j)
-                    break
-
-    tmp_paths = paths.copy()
-    paths = []
-    for j, r in enumerate(tmp_paths):
-        if j not in delete:
-            paths.append(r)
-
-    return paths
-
-
-def dict_to_atoms(index_paths, atoms):
-    trajectories = {}
-    com = atoms.get_center_of_mass()
-    for length in index_paths.keys():
-        paths = index_paths[length]
-        tmp_traj = []
-        for p in paths:
-            tmp_atoms = paths_to_atoms(atoms, [p])
-            trans = com - tmp_atoms[0].position
-            tmp_atoms.translate(trans)
-            tmp_atoms.wrap()
-            tmp_traj += [tmp_atoms]
-        trajectories[length] = tmp_traj
-
-    return trajectories
