8c96981c134a26069c131c5d8c4b9161e4d1072e
alex <ahoffman2011@gmail.com>
2025-09-14 21:54:45 -0400

    DOC: add typing and docstrings to ring_validation.py

--- zse/ring_validation.py
+++ zse/ring_validation.py
@@ -1,21 +1,23 @@
-"""
-This module contains all the various ring validation techniques implemented by
-ZSE. This is a work in progress, and more methods will be added.
+"""This module contains all the various ring validation techniques
+implemented by ZSE.
 """
 
+import math
 from collections import defaultdict
 
+import networkx as nx
 import numpy as np
+from ase import Atoms
+from ase.geometry import get_distances
 
-from zse.ring_utilities import *
-from zse.utilities import *
+from zse.ring_utilities import is_valid, shortest_valid_path
+from zse.utilities import scale_cell
 
 __all__ = ["cross_distance", "crum", "goetzke", "sastre", "sp", "sphere", "vertex"]
 
 
-def sp(G, paths):
-    """
-    Custum method for determing valid rings by ensuring for paths of 8 T-sites
+def sp(graph: nx.Graph, paths: list[list[int]]) -> list[list[int]]:
+    """Custum method for determing valid rings by ensuring for paths of 8 T-sites
     or larger, the current path is the shortest possible way to connect each
     node long the path. Alternate paths must be shorter than the current path
     to be considered invalid.
@@ -23,48 +25,58 @@ def sp(G, paths):
     7 T-site paths and smaller are handled slightly
     different where if the alrternate path is equal in length to the current
     path it is considered non valid.
-    """
 
-    import networkx as nx
+    Args:
+        graph (nx.Graph): Graph representation of the structure.
+        paths (list[list[int]]): List of paths to validate.
+
+    Returns:
+        list[list[int]]: List of valid paths.
+    """
 
     valid_paths = []
     for p in paths:
-        l = len(p)
+        length = len(p)
         flag = True
-        if l / 2 < 8:
-            for j in range(1, l - 3, 2):
-                for k in range(j + 4, l, 2):
-                    shortest_path = nx.shortest_path(G, p[j], p[k])
+        if length / 2 < 8:
+            for j in range(1, length - 3, 2):
+                for k in range(j + 4, length, 2):
+                    shortest_path = nx.shortest_path(graph, p[j], p[k])
                     for r in shortest_path:
                         if r not in p:
-                            lengths = [k - j + 1, l - k + j + 1]
+                            lengths = [k - j + 1, length - k + j + 1]
                             if len(shortest_path) < min(lengths):
                                 flag = False
                                 break
-        elif l / 2 >= 8:
-            for j in range(1, l - 3, 2):
-                for k in range(j + 2, l, 2):
-                    shortest_path = nx.shortest_path(G, p[j], p[k])
+        elif length / 2 >= 8:
+            for j in range(1, length - 3, 2):
+                for k in range(j + 2, length, 2):
+                    shortest_path = nx.shortest_path(graph, p[j], p[k])
                     for r in shortest_path:
                         if r not in p:
-                            lengths = [k - j + 1, l - k + j + 1]
+                            lengths = [k - j + 1, length - k + j + 1]
                             if len(shortest_path) < max(lengths):
                                 flag = False
                                 break
-            # if not flag:
-            #     break
+
         if flag:
             valid_paths.append(p)
 
     return valid_paths
 
 
-def sphere(atoms, paths, cutoff):
-    """
-    Method for determining valid rings by ensuring that non ring atoms are not
+def sphere(atoms: Atoms, paths: list[list[int]], cutoff: float) -> list[list[int]]:
+    """Method for determining valid rings by ensuring that non ring atoms are not
     within a cutoff distance of the center of mass of the path.
+
+    Args:
+        atoms (Atoms): ASE Atoms object of the structure.
+        paths (list[list[int]]): List of paths to validate.
+        cutoff (float): Cutoff distance to consider.
+
+    Returns:
+        list[list[int]]: List of valid paths.
     """
-    from ase.geometry import get_distances
 
     valid_paths = []
     for p in paths:
@@ -85,15 +97,19 @@ def sphere(atoms, paths, cutoff):
     return valid_paths
 
 
-def cross_distance(atoms, paths):
-    """
-    This validation method uses cross Si-Si distances of the path to determine
+def cross_distance(atoms: Atoms, paths: list[list[int]]) -> list[list[int]]:
+    """This validation method uses cross Si-Si distances of the path to determine
     if the path is a valid ring or not. The cutoffs for the cross Si-Si
     distances were determined by analyzing many valid and invalid rings.
     This method is biased by human interpretation of what constitutes a ring.
-    """
-    import math
 
+    Args:
+        atoms (Atoms): ASE Atoms object of the structure.
+        paths (list[list[int]]): List of paths to validate.
+
+    Returns:
+        list[list[int]]: List of valid paths.
+    """
     atoms = scale_cell(atoms)
 
     delete = []
@@ -112,13 +128,13 @@ def cross_distance(atoms, paths):
                     delete.append(j)
                     inner_flag = True
                     break
-            if inner_flag == False:
+            if not inner_flag:
                 outer_flag = False
                 for d in distances:
                     if d > n - math.floor(n / 6):
                         outer_flag = True
                         break
-                if outer_flag == False:
+                if not outer_flag:
                     delete.append(j)
         if n % 2 != 0 and n > 5:
             r2 = r.copy()
@@ -139,17 +155,23 @@ def cross_distance(atoms, paths):
     return paths
 
 
-def goetzke(G, index, cutoff):
+def goetzke(graph: nx.Graph, index: int, cutoff: float) -> list[list[int]]:
     """
     Method to find all cycles that cannot be decomposed into smaller cycles
     via shortcuts. This rule and algorithm was presented by:
     K. Goetzke and H.-J. Klein (https://doi.org/10.1016/0022-3093(91)90145-V)
     Slight modifications to the algorithm have been implemented to take
     advantage of symmetry.
-    """
-    import networkx as nx
 
-    sp = dict(nx.all_pairs_shortest_path_length(G, cutoff / 2))
+    Args:
+        graph (nx.Graph): Graph representation of the structure.
+        index (int): Index of the starting node.
+        cutoff (float): Maximum size of the rings to consider.
+
+    Returns:
+        list[list[int]]: List of valid paths.
+    """
+    sp = dict(nx.all_pairs_shortest_path_length(graph, cutoff / 2))
     paths = []
     for size in np.arange(6, cutoff + 1, 2):
         for i in sp[index]:
@@ -161,30 +183,56 @@ def goetzke(G, index, cutoff):
     return paths
 
 
-def get_left(cl, s1, count, sp):
+def get_left(cl: int, s1: int, count: int, sp: dict[int, dict[int, int]]) -> list[int]:
+    """Helper function for goetzke method to get left options.
+
+    Args:
+        cl (int): Current left node.
+        s1 (int): Starting left node.
+        count (int): Current count of steps remaining.
+        sp (dict[int, dict[int, int]]): Shortest path lengths dictionary.
+
+    Returns:
+        list[int]: List of valid left options.
+    """
     left_options = []
     for j1 in sp[cl]:
         q = sp[cl][j1]
         try:
             if q == 1 and sp[j1][s1] == count:
                 left_options.append(j1)
-        except:
+        except:  # noqa: E722
             pass
     return left_options
 
 
-def get_right(cr, s2, lo, count, size, sp):
+def get_right(
+    cr: int, s2: int, lo: list[int], count: int, size: int, sp: dict[int, dict[int, int]]
+) -> list[list[int]]:
+    """Helper function for goetzke method to get right options.
+
+    Args:
+        cr (int): Current right node.
+        s2 (int): Starting right node.
+        lo (list[int]): List of left options.
+        count (int): Current count of steps remaining.
+        size (int): Size of the ring being considered.
+        sp (dict[int, dict[int, int]]): Shortest path lengths dictionary.
+
+    Returns:
+        list[list[int]]: List of valid right options.
+    """
     right_options = []
-    for l in lo:
+    for l_ in lo:
         temp = []
         flag = False
         for j2 in sp[cr]:
             try:
                 q = sp[cr][j2]
-                if q == 1 and sp[j2][s2] == count and sp[l][j2] == size / 2:
+                if q == 1 and sp[j2][s2] == count and sp[l_][j2] == size / 2:
                     temp.append(j2)
                     flag = True
-            except:
+            except:  # noqa: E722, PERF203
                 pass
         if flag:
             right_options.append(temp)
@@ -193,32 +241,43 @@ def get_right(cr, s2, lo, count, size, sp):
     return right_options
 
 
-def make_path(sp, s1, s2, size):
+def make_path(sp: dict[int, dict[int, int]], s1: int, s2: int, size: int) -> list[list[int]]:
+    """Helper function for goetzke method to make paths.
+
+    Args:
+        sp (dict[int, dict[int, int]]): Shortest path lengths dictionary.
+        s1 (int): Starting left node.
+        s2 (int): Starting right node.
+        size (int): Size of the ring being considered.
+
+    Returns:
+        list[list[int]]: List of valid paths.
+    """
     count = size / 2 - 1
     left_overall = [[s2]]
     right_overall = [[s1]]
     while count > 0:
         left_temp = []
         right_temp = []
-        for left, right in zip(left_overall, right_overall):
+        for left, right in zip(left_overall, right_overall, strict=False):
             cl = left[-1]
             cr = right[-1]
             left_options = get_left(cl, s1, count, sp)
             right_options = get_right(cr, s2, left_options, count, size, sp)
 
-            for l, ro in zip(left_options, right_options):
+            for l_, ro in zip(left_options, right_options, strict=False):
                 for r in ro:
                     if r != "x":
-                        left_temp.append(left + [l])
-                        right_temp.append(right + [r])
+                        left_temp.append([*left, l_])
+                        right_temp.append([*right, r])
         left_overall = []
         right_overall = []
-        for l, r in zip(left_temp, right_temp):
-            l1 = l[-1]
+        for l_, r in zip(left_temp, right_temp, strict=False):
+            l1 = l_[-1]
             r1 = r[-1]
             flag = True
 
-            for l2, r2 in zip(left_overall, right_overall):
+            for l2, r2 in zip(left_overall, right_overall, strict=False):
                 if size / 2 % 2 == 0:
                     if l1 == r2[-1] and r1 == l2[-1]:
                         flag = False
@@ -226,20 +285,19 @@ def make_path(sp, s1, s2, size):
                     if l1 == r2[-2] and r1 == l2[-2]:
                         flag = False
             if flag:
-                left_overall.append(l)
+                left_overall.append(l_)
                 right_overall.append(r)
         count -= 1
     paths = []
-    for l, r in zip(left_overall, right_overall):
-        path = r + l
+    for l_, r in zip(left_overall, right_overall, strict=False):
+        path = r + l_
         if len(path) == size:
             paths.append(path)
     return paths
 
 
-def sastre(G, paths, index_symbol):
-    """
-    This method returns only the shortest paths rings based on the rule
+def sastre(graph: nx.Graph, paths: list[list[int]], index_symbol: str) -> list[list[int]]:
+    """This method returns only the shortest paths rings based on the rule
     presented by Sastre and Corma.
     Sastre, G; Corma, A. (DOI: 10.1021/jp8100128)
     A valid ring is a cycle that cannot be decomposed into smaller rings, and
@@ -247,21 +305,23 @@ def sastre(G, paths, index_symbol):
     neighbor oxygens in the cycle.
     Results found with this method match results from the Sastre & Corma paper.
 
-    """
-    import networkx as nx
+    Args:
+        graph (nx.Graph): Graph representation of the structure.
+        paths (list[list[int]]): List of paths to validate.
+        index_symbol (str): Element symbol of the indexing atom ("O" or "Si").
 
-    if index_symbol == "O":
-        start = 0
-    else:
-        start = 1
+    Returns:
+        list[list[int]]: List of valid paths.
+    """
+    start = 0 if index_symbol == "O" else 1
     valid_paths = []
     for p in paths:
         p2 = p + p[:4]
-        l = len(p)
+        path_length = len(p)
         for j in range(start, len(p), 2):
             flag = False
-            path, length = shortest_valid_path(G, p2[j], p2[j + 2], p2[j + 1], l)
-            if length == l:
+            _path, length = shortest_valid_path(graph, p2[j], p2[j + 2], p2[j + 1], path_length)
+            if length == path_length:
                 flag = True
                 break
         if flag:
@@ -269,52 +329,53 @@ def sastre(G, paths, index_symbol):
     return valid_paths
 
 
-def crum(G, paths, index_symbol):
-    """
-    Method to remove composite stacked rings (i.e. 8-MRs in the CHA D6R) or
+def crum(graph: nx.Graph, paths: list[list[int]], index_symbol: str) -> list[list[int]]:
+    """Method to remove composite stacked rings (i.e. 8-MRs in the CHA D6R) or
     14-MRs in the AFI framework.
-    """
-    import networkx as nx
 
-    if index_symbol == "O":
-        start = 1
-    else:
-        start = 0
-    lengths = [len(p) for p in paths]
+    Args:
+        graph (nx.Graph): Graph representation of the structure.
+        paths (list[list[int]]): List of paths to validate.
+        index_symbol (str): Element symbol of the indexing atom ("O" or "Si").
+
+    Returns:
+        list[list[int]]: List of valid paths.
+    """
+    start = 1 if index_symbol == "O" else 0
     valid_paths = []
     for path in paths:
         FLAG = False
         path2 = path + path
-        l = len(path)
-        if l > 8 and (l / 2) % 2 == 0:
-            for j in range(start, int(l / 2) - 1, 2):
-                for k in [int(j + l / 2 - 2)]:
+        length = len(path)
+        if length > 8 and (length / 2) % 2 == 0:
+            for j in range(start, int(length / 2) - 1, 2):
+                for k in [int(j + length / 2 - 2)]:
                     p1 = path2[j : k + 1]
-                    p2 = path2[k : l + j + 1]
+                    p2 = path2[k : length + j + 1]
                     l1 = len(p1)
                     l2 = len(p2)
                     if l1 < l2:
-                        G2 = G.copy()
+                        G2 = graph.copy()
                         for x in p1[1:-1]:
                             G2.remove_node(x)
                         sp = nx.shortest_path(G2, p1[0], p1[-1])
                         con1 = p2 + sp[1:-1]
-                        if len(con1) < l:
+                        if len(con1) < length:
                             FLAG = True
-                        if len(con1) == l:
-                            FLAG, q = is_valid(G, con1)
+                        if len(con1) == length:
+                            FLAG, _q = is_valid(graph, con1)
                     if l2 < l1:
-                        G2 = G.copy()
+                        G2 = graph.copy()
                         for x in p2[1:-1]:
                             G2.remove_node(x)
                         sp = nx.shortest_path(G2, p2[0], p2[-1])
                         con1 = p1 + sp[1:-1]
-                        if len(con1) < l:
+                        if len(con1) < length:
                             FLAG = True
-                        if len(con1) == l:
-                            FLAG, q = is_valid(G, con1)
+                        if len(con1) == length:
+                            FLAG, _q = is_valid(graph, con1)
                     if l1 == l2:
-                        G2 = G.copy()
+                        G2 = graph.copy()
                         sp = nx.shortest_path(G2, p1[0], p1[-1])
                         if len(sp) < l1:
                             FLAG = True
@@ -327,7 +388,16 @@ def crum(G, paths, index_symbol):
     return valid_paths
 
 
-def vertex(paths):
+def vertex(paths: list[list[int]]) -> list[list[int]]:
+    """Method to return only the shortest paths for each unique pair of
+    oxygen atoms in the path. This method is used to find vertex symbol rings.
+
+    Args:
+        paths (list[list[int]]): List of paths to validate.
+
+    Returns:
+        list[list[int]]: List of valid paths.
+    """
     oxygens = []
     v_paths = defaultdict(list)
     for p in paths:
@@ -335,14 +405,14 @@ def vertex(paths):
         oxygens.append(p[-1])
         sites = [p[1], p[-1]]
         sites.sort()
-        v_paths["{0}-{1}".format(sites[0], sites[1])].append(p)
+        v_paths[f"{sites[0]}-{sites[1]}"].append(p)
     oxygens = np.unique(oxygens)
     valid_paths = []
     for v in sorted(v_paths):
         paths = v_paths[v]
-        l = len(paths[0])
+        length = len(paths[0])
         for p in paths:
-            if len(p) == l:
-                valid_paths.append(p)
+            if len(p) == length:
+                valid_paths.append(p)  # noqa: PERF401
 
     return valid_paths
